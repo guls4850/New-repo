4f93c04e13f85ff47372b1e0d3b083b52958eafc
Gulshan Kumar <34893830+guls4850@users.noreply.github.com>
2022-08-03 22:01:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "google_storage_bucket" "GCS2" {
     "dep" = "complience"
   }
 }
+resource "google_storage_bucket" "GCS2" {
+
+  name = "newbucket53555-bucket-3"
+  storage_class = "NEARLINE"
+  location = "US-CENTRAL1"
+  labels = {
+    "env" = "tf_env"
+    "dep" = "complience"
+  }
+}
